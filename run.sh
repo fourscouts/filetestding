36a7869fe2c1632b4f29702c2635b44c5ebaf4d3
Michiel Rook <mrook@php.net>
2016-08-29 14:31:30 +0200

    Echo hostname

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 if [ -e "/data/mijn-pod-naam" ]; then
 	echo "Uit file: "
 	cat /data/mijn-pod-naam
+	echo "Hostname: "
+	echo `hostname`
 else
+	echo "Geen file: "
+	echo `hostname`
 	hostname > /data/mijn-pod-naam
 fi
 
